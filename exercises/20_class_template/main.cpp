b69ae2e37d0f1417846f908cd325a8b139b00931
DevDreamer-J <2406756703@qq.com>
2024-07-24 12:29:40 +0800

    cxxk模块

---
+++ exercises/20_class_template/main.cpp
@@ -0,0 +1,142 @@
+﻿#include "../exercise.h"
+#include <cstring>
+// READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
+
+template<class T>
+struct Tensor4D {
+    unsigned int shape[4];
+    T *data;
+
+    Tensor4D(unsigned int const shape_[4], T const *data_) {
+        // TODO: 填入正确的 shape 并计算 size
+        unsigned int size = 1;
+        for (int i = 0; i < 4; i++) {
+            size = size * shape_[i];
+            shape[i] = shape_[i];
+        }
+        data = new T[size];
+        memcpy(data, data_, size * sizeof(T));
+    }
+    ~Tensor4D() {
+        delete[] data;
+    }
+
+    // 为了保持简单，禁止复制和移动
+    Tensor4D(Tensor4D const &) = delete;
+    Tensor4D(Tensor4D &&) noexcept = delete;
+
+    // 这个加法需要支持“单向广播”。
+    // 具体来说，`others` 可以具有与 `this` 不同的形状，形状不同的维度长度必须为 1。
+    // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
+    // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
+    // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
+
+    Tensor4D &operator+=(Tensor4D const &others) {
+        // TODO: 实现单向广播的加法
+        unsigned int weidu_this[3] = {
+            this->shape[1] * this->shape[2] * this->shape[3],
+            this->shape[2] * this->shape[3],
+            this->shape[3]};
+        unsigned int weidu_others[3] = {
+            others.shape[1] * others.shape[2] * others.shape[3],
+            others.shape[2] * others.shape[3],
+            others.shape[3]};
+        for (unsigned int i = 0; i < this->shape[0] * this->shape[1] * this->shape[2] * this->shape[3]; i++) {
+            unsigned int p_this[3] = {0};
+            unsigned int p_others[3] = {0};
+            {
+                int k = i;
+                p_this[0] = k / weidu_this[1];
+                k = k % weidu_this[1];
+                p_this[1] = k / weidu_this[2];
+                k = k % weidu_this[2];
+                p_this[2] = k;
+            }
+            for (int j = 0; j < 3; j++) {
+                if (p_this[j] < weidu_others[j]) {
+                    p_others[j] = p_this[j];
+                } else {
+                    p_others[j] = weidu_others[j]-1;
+                }
+            }
+            this->data[i] = this->data[i] + others.data[p_others[0] * weidu_others[1] + p_others[1] * weidu_others[2] + p_others[2]];
+        }
+        return *this;
+    };
+};
+
+// ---- 不要修改以下代码 ----
+int main(int argc, char **argv) {
+    {
+        unsigned int shape[]{1, 2, 3, 4};
+        // clang-format off
+        int data[]{
+             1,  2,  3,  4,
+             5,  6,  7,  8,
+             9, 10, 11, 12,
+
+            13, 14, 15, 16,
+            17, 18, 19, 20,
+            21, 22, 23, 24};
+        // clang-format on
+        auto t0 = Tensor4D(shape, data);
+        auto t1 = Tensor4D(shape, data);
+        t0 += t1;
+        for (unsigned int i = 0; i < sizeof(data) / sizeof(int); i++) {
+            ASSERT(t0.data[i] == data[i] * 2, "Tensor doubled by plus its self.");
+        }
+    }
+    {
+        unsigned int s0[]{1, 2, 3, 4};
+        // clang-format off
+        float d0[]{
+            1, 1, 1, 1,
+            2, 2, 2, 2,
+            3, 3, 3, 3,
+
+            4, 4, 4, 4,
+            5, 5, 5, 5,
+            6, 6, 6, 6};
+        // clang-format on
+        unsigned int s1[]{1, 2, 3, 1};
+        // clang-format off
+        float d1[]{
+            6,
+            5,
+            4,
+
+            3,
+            2,
+            1};
+        // clang-format on
+
+        auto t0 = Tensor4D(s0, d0);
+        auto t1 = Tensor4D(s1, d1);
+        t0 += t1;
+        for (unsigned int i = 0; i < sizeof(d0) / sizeof(int); i++) {
+            ASSERT(t0.data[i] == 7.f, "Every element of t0 should be 7 after adding t1 to it.");
+        }
+    }
+    {
+        unsigned int s0[]{1, 2, 3, 4};
+        // clang-format off
+        double d0[]{
+             1,  2,  3,  4,
+             5,  6,  7,  8,
+             9, 10, 11, 12,
+
+            13, 14, 15, 16,
+            17, 18, 19, 20,
+            21, 22, 23, 24};
+        // clang-format on
+        unsigned int s1[]{1, 1, 1, 1};
+        double d1[]{1};
+
+        auto t0 = Tensor4D(s0, d0);
+        auto t1 = Tensor4D(s1, d1);
+        t0 += t1;
+        for (unsigned int i = 0; i < sizeof(d0) / sizeof(double); i++) {
+            ASSERT(t0.data[i] == d0[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
+        }
+    }
+}
---
+++ exercises/28_std_transform/main.cpp
@@ -0,0 +1,27 @@
+﻿#include "../exercise.h"
+#include <algorithm>
+#include <string>
+#include <vector>
+
+// READ: `std::transform` <https://zh.cppreference.com/w/cpp/algorithm/transform>
+// READ: `std::vector::begin` <https://zh.cppreference.com/w/cpp/container/vector/begin>
+std::string to_s(int number) {
+    return std::to_string(number * 2);
+}
+int main(int argc, char **argv) {
+    std::vector<int> val{8, 13, 21, 34, 55};
+    // TODO: 调用 `std::transform`，将 `v` 中的每个元素乘以 2，并转换为字符串，存入 `ans`
+    //std::string to_string(int& number) {
+        //return std::to_string(number * 2);
+    //}
+    std::vector<std::string> ans = std::vector<std::string>(5);
+    
+    std::transform(val.cbegin(),val.cend(),ans.begin(),to_s);
+    ASSERT(ans.size() == val.size(), "ans size should be equal to val size");
+    ASSERT(ans[0] == "16", "ans[0] should be 16");
+    ASSERT(ans[1] == "26", "ans[1] should be 26");
+    ASSERT(ans[2] == "42", "ans[2] should be 42");
+    ASSERT(ans[3] == "68", "ans[3] should be 68");
+    ASSERT(ans[4] == "110", "ans[4] should be 110");
+    return 0;
+}
